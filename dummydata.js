a169ed8d7f4ed4fdafce5447ef811a692f09699b
name <holmesvaldezz@gmail.com>
2024-03-05 14:35:09 +0800

    Read Dummy Data

---
+++ dummydata.js
@@ -0,0 +1,41 @@
+
+
+/* user = [
+    {
+        "id": "1",
+        "Fname": "Holmes",
+        "Lname": "Valdez",
+        "Age": "21"
+    }
+] */
+
+list = [
+    {
+            "id": "1",
+            "taskDes": "Learn ERD Diagram ",
+            "taskDeadLine": "Monday"
+    },
+
+    {
+        "id": "2",
+        "taskDes": "Learn DBMS QUERY!",
+        "taskDeadLine": "Tuesday"
+    },
+
+    {
+        "id": "3",
+        "taskDes": "Learn Read Dummy Data",
+        "taskDeadLine" : "Wednesday"
+    }
+
+
+    
+]
+
+
+
+
+/* console.log('Successfully read Data', (user), (list) );
+console.log(user); */
+
+//console.log(list);
