0ab63fb78477a2d2a26e149f33bc80fe29d42660
YahboiHolmes <149929671+yahboiholmes@users.noreply.github.com>
2024-03-06 14:57:51 +0800

    Add files via upload

---
+++ Assigned-task/task-2/createdatadummy.js
@@ -0,0 +1,8 @@
+
+var list = {
+    "id": "1",
+    "tododate": "Monday",
+    "task": "Learn to create a data"
+}
+
+module.exports =list;
---
+++ Assigned-task/task-3/readdatadummy.js
@@ -0,0 +1,5 @@
+const express = require('express')
+
+const read_Data = require('./Assigned-task/task-2/createdatadummy.js');
+
+console.log(read_Data);
